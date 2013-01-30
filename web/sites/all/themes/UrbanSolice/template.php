2937c61e359d246d0a81ae0dfe001f6bc9d5f1f4
Elias Rivera <eliasriv@gmail.com>
2013-01-30 15:21:54 -0500

    First commit ecuenvases.com

---
+++ web/sites/all/themes/UrbanSolice/template.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Set count variables so column numbers can be dynamic.
+ */
+function urbansolice_preprocess_page (&$vars) {
+  $vars['preface'] = count(array_filter(array($vars['page']['preface_one'], $vars['page']['preface_two'], $vars['page']['preface_three'])));
+  $vars['bottom'] = count(array_filter(array($vars['page']['bottom_one'], $vars['page']['bottom_two'], $vars['page']['bottom_three'], $vars['page']['bottom_four'])));
+  // Display user account links.
+  $vars['user_links'] = _urbansolice_user_links();
+  //dpm($vars);
+}
+
+/**
+ * User/account related links.
+ */
+function _urbansolice_user_links() {
+  // Add user-specific links
+  global $user;
+  $user_links = array();
+  if (empty($user->uid)) {
+    $user_links['login'] = array('title' => t('Login'), 'href' => 'user');
+    // Do not display register link if registration is not allowed.
+    if (variable_get('user_register', 1)) {
+      $user_links['register'] = array('title' => t('Register'), 'href' => 'user/register');
+    }
+  }
+  else {
+    $user_links['account'] = array('title' => t('Hello @username', array('@username' => $user->name)), 'href' => 'user', 'html' => TRUE);
+    $user_links['logout'] = array('title' => t('Logout'), 'href' => "logout");
+  }
+  return $user_links;
+}
+
+/**
+* Customize theme
+*/
+function urban_solice_theme($existing, $type, $theme, $path) {
+  $items = array(); 
+
+  // create custom user-login.tpl.php
+  $items['user_login'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'urban_solice') . '/templates',
+    'template' => 'user--login',
+    'preprocess functions' => array(
+    'urbansolice_preprocess_user_login'
+    ),
+  );
+
+  return $items;
+}
---
+++ web/sites/all/themes/UrbanSolice/templates/page--catalogo.tpl.php
@@ -0,0 +1,145 @@
+<?php
+?>
+
+  <div id="page-wrapper">
+    <div id="page">
+  
+      <div id="header"><div class="container section header clearfix">
+  
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo" class="logo">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          </a>
+        <?php endif; ?>
+
+        <?php if ($site_name || $site_slogan): ?>
+          <div id="name-and-slogan">
+            <?php if ($site_name): ?>
+              <?php if ($title): ?>
+                <div id="site-name"><strong>
+                  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+                </strong></div>
+              <?php else: /* Use h1 when the content title is empty */ ?>
+                <h1 id="site-name">
+                  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+                </h1>
+              <?php endif; ?>
+            <?php endif; ?>  
+          </div> <!-- /#name-and-slogan -->
+        <?php endif; ?>
+          <?php print render($page['header']); ?>
+      </div><!-- /.section .header -->
+
+      <?php if ($main_menu || $secondary_menu): ?>
+        <div id="navigation">
+          <div class="container navigation section">
+            <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'clearfix')))); ?>
+          </div><!-- /.section .navigation -->
+        </div> <!-- /#navigation -->
+      <?php endif; ?>
+      
+      </div> <!-- /#header -->
+  
+      <div id="banner-wrap" class="slider-content <?php if(!$page['banner']) print 'empty' ?>">
+        <?php if ($page['banner']): ?>
+             <div id="banner" class="clearfix">
+               <div class="container region">
+                 <?php print render ($page['banner']); ?>
+               </div>
+             </div>
+        <?php endif; ?>
+      </div>
+  
+  
+      <div id="main-wrapper">
+        <?php if ($page['preface_one'] || $page['preface_two'] || $page['preface_three']): ?>
+          <div id="preface" class="clearfix front-page-sections">
+            <div class="container preface clearfix">
+               <?php if ($page['preface_one']): ?>
+              <div class="section preface-one<?php print' preface-'.  $preface; ?>">
+                <div class="gutter">
+                  <?php print render($page['preface_one']); ?>
+                </div>
+              </div>
+              <?php endif; ?>
+              <?php if ($page['preface_two']): ?>
+              <div class="section preface-two<?php print' preface-'.  $preface; ?>">
+                <div class="gutter">
+                  <?php print render($page['preface_two']); ?>
+                </div>
+              </div>
+              <?php endif; ?>
+              <?php if ($page['preface_three']): ?>
+              <div class="section preface-three<?php print' preface-'. $preface; ?>">
+                <div class="gutter">
+                  <?php print render($page['preface_three']); ?>
+                </div>
+              </div>
+              <?php endif; ?>
+            </div>
+          </div>
+        <?php endif; ?>
+      </div> <!-- /#content -->
+    </div><!-- /#page -->
+    
+    <div id="footer">
+      <?php if ($page['bottom_one'] || $page['bottom_two'] || $page['bottom_three'] || $page['bottom_four']): ?>
+      <div id="bottom" class="container clearfix">
+        <?php if ($page['bottom_one']): ?>
+          <div class="region-bottom-one-user-login">
+            <div class="region bottom bottom-one<?php print ' bottom-' .  $bottom; ?>">
+              <div class="gutter">
+                <?php 
+                  print render($page['bottom_one']);
+                ?>
+              </div>
+            </div>
+
+            <div class="user-login-block">
+              <h2>Menu Usuario</h2>
+              <?php 
+                if (empty($user->uid)){
+                  print render(drupal_get_form('user_login_block'));
+                }
+                else
+                {
+                  print l('Log out', 'user/logout');
+                }
+              ?>
+            </div>
+          </div>
+        <?php endif; ?>
+        <?php if ($page['bottom_two']): ?>
+          <div class="region bottom bottom-two<?php print ' bottom-' .  $bottom; ?>">
+            <div class="gutter">
+              <?php print render($page['bottom_two']); ?>
+            </div>
+          </div>
+        <?php endif; ?>
+        <?php if ($page['bottom_three']): ?>
+          <div class="region bottom bottom-three<?php print ' bottom-' . $bottom; ?>">
+            <div class="gutter">
+              <?php print render($page['bottom_three']); ?>
+            </div>
+          </div>
+        <?php endif; ?>
+        <?php if ($page['bottom_four']): ?>
+          <div class="region bottom bottom-four<?php print ' bottom-'. $bottom; ?>">
+            <div class="gutter">
+              <?php print render($page['bottom_four']); ?>
+            </div>
+          </div>
+        <?php endif; ?>
+      </div>
+      <?php endif; ?>
+      <div class="container section footer">
+        <?php print render($page['footer']); ?>
+        <div id="levelten">
+          Developed by Elías Rivera
+          <br>
+          e-mail: eliasriv@gmail.com
+        </div>
+      </div><!-- /.section -->
+    </div> <!-- /#footer -->
+    
+  </div> <!-- /#page-wrapper -->
